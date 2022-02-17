2d2f7eb7478e8e5b9d9509135b9831db7e7806db
jannik1995 <50104987+jannik1995@users.noreply.github.com>
2022-02-17 14:45:35 +0100

    test footer

--- pages/index.php
+++ pages/index.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
+<?php include 'header.php' ?>
 <h1>Hello World</h1>
 <?php include 'footer.php' ?>
 </body>
