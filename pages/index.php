a04b26dbe81b59a67833e57603da75eccd3f7559
andreasrohr <andreas@teqly.ch>
2022-02-17 14:59:01 +0100

    style path

--- pages/index.php
+++ pages/index.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/Website/style.css" media="all" type="text/css">
+
+
 </head>
 <body>
 <?php include 'header.php' ?>
 <h1>Hello World</h1>
+
+
+<a id="test">sdsdsd</a>
+
 <?php include 'footer.php' ?>
 </body>
 </html>
-<script type="text/javascript" src="/./script.js"></script>
+<script type="text/javascript" src="/Website/script.js"></script>
