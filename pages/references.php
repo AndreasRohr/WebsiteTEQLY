59af4c7d88ed8921bd951380058a29268487a4e3
andreasrohr <andreas@teqly.ch>
2022-03-23 20:12:41 +0100

    add clickable reference buttons

--- pages/references.php
+++ pages/references.php
@@ -44,41 +44,95 @@
 <h3>Netzwerk & Support</h3>
 
 <div class="collapse-row">
-<div class="collapse-card">
-    <button class="collapsible">Bäckerei Meier</button>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <div class="collapse-card">
+        <button id="1" class="collapsible">Bäckerei Meier</button>
     </div>
-</div>
 
-<div class="collapse-card">
-    <button class="collapsible">Guesthouse Lelocle</button>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <div class="collapse-card">
+        <button id="2" class="collapsible">Guesthouse Lelocle</button>
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
     </div>
-</div>
 
-<div class="collapse-card">
-    <button class="collapsible">Emma Bed&Breakfast</button>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <div class="collapse-card">
+        <button id="3" class="collapsible">Emma Bed&Breakfast</button>
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
     </div>
 </div>
-</div>
+<div id="content" class="content"></div>
 
 <script>
     var coll = document.getElementsByClassName("collapsible");
     var i;
 
+    var emma = "emma";
+    var meier = "meier";
+    var lelocle = "lelocle";
+
+
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
             this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
+            var content = document.getElementById("content");
+
+            switch (this.id) {
+                case "1":
+                    content.innerHTML = meier;
+
+                    if(document.getElementById("1").classList.contains("active")){
+                        content.style.maxHeight = content.scrollHeight + "px";
+                    }else{
+                        content.style.maxHeight = null;
+                    }
+
+                    if(document.getElementById("2").classList.contains("active")){
+                        document.getElementById("2").classList.toggle("active");
+                    }
+                    if(document.getElementById("3").classList.contains("active")){
+                        document.getElementById("3").classList.toggle("active");
+                    }
+
+                    break;
+                case "2":
+                    content.innerHTML = lelocle;
+
+                    if(document.getElementById("2").classList.contains("active")){
+                        content.style.maxHeight = content.scrollHeight + "px";
+                    }else{
+                        content.style.maxHeight = null;
+                    }
+
+                    if(document.getElementById("1").classList.contains("active")){
+                        document.getElementById("1").classList.toggle("active");
+                    }
+                    if(document.getElementById("3").classList.contains("active")){
+                        document.getElementById("3").classList.toggle("active");
+                    }
+
+                    break;
+                case "3":
+                    content.innerHTML = emma;
+                    if(document.getElementById("3").classList.contains("active")){
+                        content.style.maxHeight = content.scrollHeight + "px";
+                    }else{
+                        content.style.maxHeight = null;
+                    }
+
+                    if(document.getElementById("1").classList.contains("active")){
+                        document.getElementById("1").classList.toggle("active");
+                    }
+                    if(document.getElementById("2").classList.contains("active")){
+                        document.getElementById("2").classList.toggle("active");
+                    }
+
+                    break;
+                default:
+                    break;
             }
+
         });
     }
 </script>
