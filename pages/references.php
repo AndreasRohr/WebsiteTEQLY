5093284bdfec011da06e39ba698a57fe95dbd99e
jannik1995 <50104987+jannik1995@users.noreply.github.com>
2022-03-23 19:23:45 +0100

    References

--- pages/references.php
+++ pages/references.php
@@ -1,87 +1,49 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        .collapsible {
-            background-color: #777;
-            color: white;
-            cursor: pointer;
-            padding: 18px;
-            width: 100%;
-            border: none;
-            text-align: left;
-            outline: none;
-            font-size: 15px;
-            height: 100px;
-        }
+    <title>Referenzen</title>
+    <link rel="stylesheet" href="/Website/normalize.css" media="all" type="text/css">
+    <link rel="stylesheet" href="/Website/style.css" media="all" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
-        .active, .collapsible:hover {
-            background-color: #555;
-        }
-
-        .collapse-row{
-            display: flex;
-        }
-
-        .collapse-card{
-            width: 33%;
-        }
-
-        .content {
-            padding: 0 18px;
-            max-height: 0;
-            overflow: hidden;
-            transition: max-height 0.2s ease-out;
-            background-color: #f1f1f1;
-        }
-    </style>
 </head>
+
 <body>
-
+<?php include 'header.php' ?>
 <h2>Unsere Referenzen</h2>
-
 <h3>Netzwerk & Support</h3>
 
 <div class="collapse-row">
-<div class="collapse-card">
-    <button class="collapsible">Bäckerei Meier</button>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <div class="collapse-card" style="min-width: 33%; display: block">
+        <button href="#demo1" class="collapsible" data-toggle="collapse">Bäckerei Meier</button>
+    </div>
+    <div class="collapse-card" style="min-width: 33%; display: block">
+        <button href="#demo2" class="collapsible" data-toggle="collapse">Vinesch</button>
+    </div>
+    <div class="collapse-card" style="min-width: 33%; display: block">
+        <button href="#demo3" class="collapsible" data-toggle="collapse">Inselspital</button>
     </div>
 </div>
-
-<div class="collapse-card">
-    <button class="collapsible">Guesthouse Lelocle</button>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
+<div id="demo1" class="collapse">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</div>
+<div id="demo2" class="collapse">
+   TEST
+</div>
+<div id="demo3" class="collapse">
+   TEST 2
 </div>
 
-<div class="collapse-card">
-    <button class="collapsible">Emma Bed&Breakfast</button>
-    <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-</div>
-</div>
-
-<script>
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
-
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
-    }
-</script>
 
 </body>
 </html>
+<script>$('.collapsible').on('click',function(){$('.collapse').collapse('hide');})</script>
+
+
