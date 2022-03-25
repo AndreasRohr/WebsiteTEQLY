45e3be6425872b631e39455f7ca1a9e8861d9d11
andreasrohr <andreas@teqly.ch>
2022-03-25 14:19:02 +0100

    add all references

--- pages/references.php
+++ pages/references.php
@@ -1,10 +1,85 @@
+<!--
+Vorlage für weitere Referenzen
+
+---HTML---
+
+    <div class="collapse-row">
+        <div class="collapse-card">
+
+            ****ID setzen: kategorie-aufsteigendeNummer
+            ****CLASS: collapsible-kategoriereihe  (im Beispiel: n(etwork)r(ow)1)
+            ****ONCLICK: descKategorieReihe(name für text, gesetzt in JS, this.id)
+
+            <button id="network-1" class="collapsible-nr1" onClick="descNetworkR1(baeckereiMeier, this.id)">Bäckerei Meier</button>
+        </div>
+
+        <div class="collapse-card">
+            <button id="network-2" class="collapsible-nr1" onClick="descNetworkR1(guesthouseLelocle, this.id)">Guesthouse Lelocle</button>
+        </div>
+
+        <div class="collapse-card">
+            <button id="network-3" class="collapsible-nr1" onClick="descNetworkR1(emmaBnb, this.id)">Emma Bed&Breakfast</button>
+        </div>
+    </div>
+
+   ****ID setzen: content-kategoriereihe (im Beispiel: n(etwork)r(ow)1)
+    <div id="content-nr1" class="content">
+        <p></p>
+    </div>
+
+
+---JS---
+ **** Variabel für Text (Name der Variabel muss mit onclick im HTML übereinstimmen)
+ var baeckereiMeier = "text für meier";
+
+ **** Variabel für zuletzt gedrückter Button
+ var lastButtonPressedNR1;
+
+
+ **** Name der Funktion: descKategorieReihe
+ function descNetworkR1(text, btnid){
+
+        ****GetElement ID anpassen (gesetzt im HTML)
+
+        var content = document.getElementById("content-nr1");
+        content.innerHTML = text;
+
+        ****QuuerySelector anpassen (gesetzt im HTML)
+        var btns = document.querySelectorAll(".collapsible-nr1");
+
+        btns.forEach(el => {
+            if(el.classList.contains("active")){
+                el.classList.toggle("active");
+            }
+        })
+
+        ****lastButton anpassen
+        if(lastButtonPressedNR1 === btnid){
+            content.style.maxHeight = null;
+            document.getElementById(btnid).classList.toggle("active");
+            lastButtonPressed1 = null;
+        }else{
+            content.style.maxHeight = content.scrollHeight + "px";
+            lastButtonPressed1 = btnid;
+        }
+
+        document.getElementById(btnid).classList.toggle("active");
+    }
+
+-->
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TEQLY Referenzen</title>
     <style>
-        .collapsible1, .collapsible2 {
+
+        [class^="collapsible-"] {
             background-color: #777;
             color: white;
             cursor: pointer;
@@ -17,7 +92,7 @@
             height: 100px;
         }
 
-        .active, .collapsible1:hover, .collapsible2:hover {
+        .active, [class^="collapsible-"]:hover {
             background-color: #555;
         }
 
@@ -49,40 +124,100 @@
 
     <div class="collapse-row">
         <div class="collapse-card">
-            <button id="1" class="collapsible1" onClick="addDesc1(meier, this.id)">Bäckerei Meier</button>
+            <button id="network-1" class="collapsible-nr1" onClick="descNetworkR1(baeckereiMeier, this.id)">Bäckerei Meier</button>
         </div>
 
         <div class="collapse-card">
-            <button id="2" class="collapsible1" onClick="addDesc1(lelocle, this.id)">Guesthouse Lelocle</button>
+            <button id="network-2" class="collapsible-nr1" onClick="descNetworkR1(guesthouseLelocle, this.id)">Guesthouse Lelocle</button>
         </div>
 
         <div class="collapse-card">
-            <button id="3" class="collapsible1" onClick="addDesc1(emma, this.id)">Emma Bed&Breakfast</button>
+            <button id="network-3" class="collapsible-nr1" onClick="descNetworkR1(emmaBnb, this.id)">Emma Bed&Breakfast</button>
         </div>
     </div>
-    <div id="content1" class="content">
+    <div id="content-nr1" class="content">
         <p></p>
     </div>
 
 
     <div class="collapse-row">
         <div class="collapse-card">
-            <button id="4" class="collapsible2" onClick="addDesc2(meier, this.id)">Bäckerei Meier</button>
+            <button id="network4" class="collapsible-nr2" onClick="descNetworkR2(kulturhofHinteregg, this.id)">Kulturhof Hinteregg</button>
         </div>
 
         <div class="collapse-card">
-            <button id="5" class="collapsible2" onClick="addDesc2(lelocle, this.id)">Guesthouse Lelocle</button>
+            <button id="network5" class="collapsible-nr2" onClick="descNetworkR2(artOfMetal, this.id)">ArtOfMetal</button>
         </div>
 
         <div class="collapse-card">
-            <button id="6" class="collapsible2" onClick="addDesc2(emma, this.id)">Emma Bed&Breakfast</button>
+            <button id="network6" class="collapsible-nr2" onClick="descNetworkR2(grundgut, this.id)">Grundgut</button>
         </div>
     </div>
-    <div id="content2" class="content">
+    <div id="content-nr2" class="content">
         <p></p>
     </div>
 
 
+    <h3>Rental</h3>
+    <div class="collapse-row">
+        <div class="collapse-card">
+            <button id="rental1" class="collapsible-rr1" onClick="descRentalR1(xana, this.id)">XANA</button>
+        </div>
+
+        <div class="collapse-card">
+            <button id="rental2" class="collapsible-rr1" onClick="descRentalR1(kantonBern, this.id)">Kanton Bern</button>
+        </div>
+    </div>
+    <div id="content-rr1" class="content">
+        <p></p>
+    </div>
+
+
+    <h3>Personalverleih</h3>
+    <div class="collapse-row">
+        <div class="collapse-card">
+            <button id="lease1" class="collapsible-lr1" onClick="descLeaseR1(eventOn, this.id)">EventOn</button>
+        </div>
+    </div>
+    <div id="content-lr1" class="content">
+        <p></p>
+    </div>
+
+
+
+    <h3>Webauftritte</h3>
+    <div class="collapse-row">
+        <div class="collapse-card">
+            <button id="homepage1" class="collapsible-hr1" onClick="descHomepageR1(coreMio, this.id)">Ristorante CoreMio</button>
+        </div>
+
+        <div class="collapse-card">
+            <button id="homepage2" class="collapsible-hr1" onClick="descHomepageR1(savoldi, this.id)">Savoldi</button>
+        </div>
+
+    </div>
+    <div id="content-hr1" class="content">
+        <p></p>
+    </div>
+
+
+    <h3>Custom Solutions</h3>
+    <div class="collapse-row">
+        <div class="collapse-card">
+            <button id="custom1" class="collapsible-cr1" onClick="descCustomR1(inselspitalBern, this.id)">Inselspital Bern</button>
+        </div>
+
+        <div class="collapse-card">
+            <button id="custom2" class="collapsible-cr1" onClick="descCustomR1(praxisDZ, this.id)">Praxis DZ</button>
+        </div>
+
+        <div class="collapse-card">
+            <button id="custom3" class="collapsible-cr1" onClick="descCustomR1(scBern, this.id)">SC Bern</button>
+        </div>
+    </div>
+    <div id="content-cr1" class="content">
+        <p></p>
+    </div>
 
 
 
@@ -91,24 +226,48 @@
 
 
 <script>
+    //Texte für Referenzen - Netzwerk und Support
+    var baeckereiMeier = "text für meier";
+    var guesthouseLelocle = "text für lelocle";
+    var emmaBnb = "text für emma";
+    var kulturhofHinteregg = "text für kulturhof";
+    var artOfMetal = "text für artofmetal";
+    var grundgut = "text für grundgut";
 
-    var emma = "emma";
-    var meier = "meier";
-    var lelocle = "lelocle";
+    //Texte für Personalverleih
+    var eventOn = "text für eventon"
+
+    //Texte für Referenzen - Webauftritte
+    var coreMio = "Text für CoreMio"
+    var savoldi = "Text für savoldi"
+
+    //Texte für Custom Solutions
+    var praxisDZ = "Text für DZ"
+    var inselspitalBern = "Text für Inselspital"
+    var scBern = "Text für sc bern"
+
+    //Texte für Rental
+    var xana = "Text für xana"
+    var kantonBern = "Text für Kanton Bern"
+
+    //zuletzt gedrückter Button jeweiliger Reihe
+    //N - Network, R - Rental, L - Lease, H - Homepage, C - Custom
+    var lastButtonPressedNR1;
+    var lastButtonPressedNR2;
+    var lastButtonPressedRR1;
+    var lastButtonPressedLR1;
+    var lastButtonPressedHR1;
+    var lastButtonPressedCR1;
+
     var text
     var btnid;
-    var lastButtonPressed1;
-    var lastButtonPressed2;
 
-    function addDesc1(text, btnid){
 
-        var content = document.getElementById("content1");
+    function descNetworkR1(text, btnid){
 
+        var content = document.getElementById("content-nr1");
         content.innerHTML = text;
-
-        //  document.getElementById("content1").innerHTML=text;
-
-        var btns = document.querySelectorAll(".collapsible1");
+        var btns = document.querySelectorAll(".collapsible-nr1");
 
         btns.forEach(el => {
             if(el.classList.contains("active")){
@@ -116,25 +275,22 @@
             }
         })
 
-        if(lastButtonPressed1 === btnid){
+        if(lastButtonPressedNR1 === btnid){
             content.style.maxHeight = null;
             document.getElementById(btnid).classList.toggle("active");
-            lastButtonPressed1 = null;
+            lastButtonPressedNR1 = null;
         }else{
             content.style.maxHeight = content.scrollHeight + "px";
-            lastButtonPressed1 = btnid;
+            lastButtonPressedNR1 = btnid;
         }
-
         document.getElementById(btnid).classList.toggle("active");
     }
 
-    function addDesc2(text, btnid){
+    function descNetworkR2(text, btnid){
 
-        var content = document.getElementById("content2");
-
+        var content = document.getElementById("content-nr2");
         content.innerHTML=text;
-
-        var btns = document.querySelectorAll(".collapsible2");
+        var btns = document.querySelectorAll(".collapsible-nr2");
 
         btns.forEach(el => {
             if(el.classList.contains("active")){
@@ -142,19 +298,115 @@
             }
         })
 
-        if(lastButtonPressed2 === btnid){
+        if(lastButtonPressedNR2 === btnid){
             content.style.maxHeight = null;
             document.getElementById(btnid).classList.toggle("active");
-            lastButtonPressed2 = null;
+            lastButtonPressedNR2 = null;
         }else{
             content.style.maxHeight = content.scrollHeight + "px";
-            lastButtonPressed2 = btnid;
+            lastButtonPressedNR2 = btnid;
         }
-
         document.getElementById(btnid).classList.toggle("active");
-
-
     }
+
+    function descRentalR1(text, btnid){
+
+        var content = document.getElementById("content-rr1");
+        content.innerHTML=text;
+        var btns = document.querySelectorAll(".collapsible-rr1");
+
+        btns.forEach(el => {
+            if(el.classList.contains("active")){
+                el.classList.toggle("active");
+            }
+        })
+
+        if(lastButtonPressedRR1 === btnid){
+            content.style.maxHeight = null;
+            document.getElementById(btnid).classList.toggle("active");
+            lastButtonPressedRR1 = null;
+        }else{
+            content.style.maxHeight = content.scrollHeight + "px";
+            lastButtonPressedRR1 = btnid;
+        }
+        document.getElementById(btnid).classList.toggle("active");
+    }
+
+    function descLeaseR1(text, btnid){
+
+        var content = document.getElementById("content-lr1");
+        content.innerHTML=text;
+        var btns = document.querySelectorAll(".collapsible-lr1");
+
+        btns.forEach(el => {
+            if(el.classList.contains("active")){
+                el.classList.toggle("active");
+            }
+        })
+
+        if(lastButtonPressedLR1 === btnid){
+            content.style.maxHeight = null;
+            document.getElementById(btnid).classList.toggle("active");
+            lastButtonPressedLR1 = null;
+        }else{
+            content.style.maxHeight = content.scrollHeight + "px";
+            lastButtonPressedLR1 = btnid;
+        }
+        document.getElementById(btnid).classList.toggle("active");
+    }
+
+    function descHomepageR1(text, btnid){
+
+        var content = document.getElementById("content-hr1");
+        content.innerHTML=text;
+        var btns = document.querySelectorAll(".collapsible-hr1");
+
+        btns.forEach(el => {
+            if(el.classList.contains("active")){
+                el.classList.toggle("active");
+            }
+        })
+
+        if(lastButtonPressedHR1 === btnid){
+            content.style.maxHeight = null;
+            document.getElementById(btnid).classList.toggle("active");
+            lastButtonPressedHR1 = null;
+        }else{
+            content.style.maxHeight = content.scrollHeight + "px";
+            lastButtonPressedHR1 = btnid;
+        }
+        document.getElementById(btnid).classList.toggle("active");
+    }
+
+
+
+    function descCustomR1(text, btnid){
+
+        var content = document.getElementById("content-cr1");
+        content.innerHTML=text;
+        var btns = document.querySelectorAll(".collapsible-cr1");
+
+        btns.forEach(el => {
+            if(el.classList.contains("active")){
+                el.classList.toggle("active");
+            }
+        })
+
+        if(lastButtonPressedCR1 === btnid){
+            content.style.maxHeight = null;
+            document.getElementById(btnid).classList.toggle("active");
+            lastButtonPressedCR1 = null;
+        }else{
+            content.style.maxHeight = content.scrollHeight + "px";
+            lastButtonPressedCR1 = btnid;
+        }
+        document.getElementById(btnid).classList.toggle("active");
+    }
+
+
+
+
+
 
 </script>
 
