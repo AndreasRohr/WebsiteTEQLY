9ba285de7887de5ec507773f4967c09f6219595e
andreasrohr <andreas@teqly.ch>
2022-03-25 16:35:31 +0100

    add contact page

---
+++ pages/contact.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/Website/normalize.css" media="all" type="text/css">
+    <link rel="stylesheet" href="/Website/style.css" media="all" type="text/css">
+    <title>Startseite</title>
+</head>
+
+<body>
+<?php include 'header.php' ?>
+<div id="main" style="margin-top: 4rem">
+    <h1 class="contact-title">TEQLY GmbH</h1>
+    <h2 class="contact-subtitle">Badenerstrasse 13</h2>
+    <h2 class="contact-subtitle">5200 Brugg</h2>
+    <h2 class="contact-subtitle">info@teqly.ch</h2>
+    <h2 class="contact-subtitle">056 511 11 33</h2>
+
+    <div class="index-container">
+
+        <div class="lineblack-index"></div>
+        <div class="contact-col-top">
+            <h1>Sandro Bütler</h1>
+        </div>
+        <div class="contact-wrapper">
+            <div class="contact-col-left">
+                <p>sandro@teqly.ch<br>
+                    056 511 11 30
+                </p>
+                <p>Kontaktdaten speichern (///DOWNLOAD VCARD)</p>
+            </div>
+        </div>
+
+        <div class="lineblack-index"></div>
+        <div class="contact-col-top">
+            <h1>Jannik Frei</h1>
+        </div>
+        <div class="contact-wrapper">
+            <div class="contact-col-left">
+                <p>
+                    jannik@teqly.ch<br>
+                    056 511 11 31
+                </p>
+                <p>Kontaktdaten speichern</p>
+            </div>
+        </div>
+
+        <div class="lineblack-index"></div>
+        <div class="contact-col-top">
+            <h1>Andreas Rohr</h1>
+        </div>
+        <div class="contact-wrapper">
+            <div class="contact-col-left">
+                <p>
+                    andreas@teqly.ch <br>
+                    056 511 11 32
+                </p>
+                <p>Kontaktdaten speichern</p>
+            </div>
+
+        </div>
+
+
+    </div>
+</div>
+<br>
+<?php include 'footer.php' ?>
+</body>
+</html>
+<script type="text/javascript" src="/Website/script.js"></script>
