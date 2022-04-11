e1c920a4f49e93dca655c33aa96c83067744ff1c
jannik1995 <50104987+jannik1995@users.noreply.github.com>
2022-04-11 21:13:06 +0200

    Jobs, Contact, vCards, references2

---
+++ pages/references2.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'header_imports.php' ?>
+    <title>Startseite</title>
+    <?php include 'header.php' ?>
+</head>
+<body>
+
+<div id="main" style="margin-top: 7rem">
+    <div class="title-row">
+        <div class="title-left">
+            <div class="typewriter">
+                <h1 class="indextitle">Unsere Referenzen</h1>
+            </div>
+        </div>
+    </div>
+    <div class="index-container" style="margin-top: 4rem">
+        <div  class="title-right">
+            <h2>Willkommen bei TEQLY.<br>Wir sind Ihre Ansprechpartner wenn es um innovative und kundenorientiert IT-Lösungen geht.</h2>
+        </div>
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>In einer komplex vernetzten Welt ist es nicht einfach den Überblick zu behalten. Bei der Digitalisierung stehenzubleiben, kommt einem Rückschritt gleich. Wir sind der Partner für Ihr Unternehmen wenn es um Herausforderungen in der digitalen Welt geht. Unsere Dienstleistungspalette ist vielschichtig und deckt alle Bedürfnisse eines Unternehmens ab. Egal ob Beratung, Schulung, Support, Hard- oder Software - Wir stehen Ihnen gerne zur Seite, damit Sie sich wieder auf Ihr Kerngeschäft konzentrieren können. <a href="/Website/pages/products.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">WEBAUFTRITT 09/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_coremio.php">Ristorante CoreMio</a>
+        </div>
+        <div class="index-col-right">
+            <p>Schon beim Anblick der Website coremio.ch steigt die Vorfreude auf die italienische Küche. Wir durften für das neugegründete Restaurant in Aarau einen Webauftritt umsetzen. Besonderes Augenmerk wurde auf die Präsentation des Menus gelegt. Zudem wurde eine teilautomatisierte Reservationsmöglichkeit verbaut, die das Team im Restaurantalltag entlasten wird.<a href="/Website/pages/references_coremio.php">Mehr erfahren.</a></p>
+        </div>
+
+
+        <div class="lineblack-index"></div>
+        <h2 class="references-category">NETZWERK 08/2020</h2>
+        <div class="index-col-left">
+            <a href="/Website/pages/references_meier_2020.php">Bäckerei Meier</a>
+        </div>
+        <div class="index-col-right">
+            <p>Die Bäckerei und Confiserie Meier AG in Mägenwil ist ein innovatives Traditionsunternehmen. Im Laufe der Jahre ist die IT-Infrastruktur mit dem Unternehmen gewachsen und wurde stetig grösser. TEQLY hat das bestehende Netzwerk analysiert und ist zum Schluss gekommen, dass viele Clients nicht mehr aktiv waren und ausgemistet werden könnten. Zusammen mit dem Verkauf von einigen neuen Komponenten wurde das Netz an einem Wochenende (teilweise während laufendem Betrieb) ausgemistet und teilweise erneuert. Auch die bestehenden Lan- und WLan-Netze wurden neu konfiguriert und so sicher und stabil für die Zukunft ausgelegt. Wir freuen uns die Bäckerei und Confiserie Meier AG auch in Zukunft als IT-Partner begleiten zu dürfen. <a href="/Website/pages/references_meier_2020.php">Mehr erfahren.</a></p>
+        </div>
+    </div>
+</div>
+<br>
+<?php include 'footer.php' ?>
+</body>
+</html>
+<script type="text/javascript" src="/Website/script.js"></script>
+<script>
+    // Random String return for index.php
+    var items = Array("Ihre IT-Partner.", "Ihre Problemlöser.", "Ihre Supporter.", "Ihre Programmierer.", "Ihre IT-Berater.", "Ihre Digitalisierer.");
+
+    window.onload = function answerWriter() {
+        document.getElementById("titleh1").innerHTML = randString();
+    }
+    function randString() {
+        var item = items[Math.floor(Math.random() * items.length)];
+        return item;
+    }
+</script>
+
