8315ca2b5b449162baacc265b031e022349112da
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2022-04-11 15:42:01 +0200

    Merge branch 'main' of https://gitlab.com/teqly1/Website.git

---
+++ pages/references_coremio.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'header_imports.php' ?>
+    <!--EDIT HERE-->
+    <title>References</title>
+    <?php include 'header.php' ?>
+</head>
+<body>
+
+<div id="main" style="margin-top: 7rem">
+    <div class="title-row">
+        <div class="title-left">
+            <div class="typewriter">
+                <!--EDIT HERE-->
+                <h2 class="references-category">Website</h2>
+                <!--EDIT HERE-->
+                <h1  class="indextitle">Ristorante CoreMio</h1>
+            </div>
+        </div>
+    </div>
+    <div class="index-container" style="margin-top: 4rem">
+        <img src="/Website/assets/ref_meier_1.jpg">
+        <div  class="title-right">
+            <!--EDIT HERE-->
+            <h2>Website<br>09/2022<br>Webdesign inkl. Reservationstool.</h2>
+        </div>
+        <div class="lineblack-index"></div>
+        <div class="header-row"style="grid-template-columns: repeat(2,auto);">
+        <div class="index-col-left" style="max-width: 20rem;white-space: normal">
+            <!--EDIT HERE-->
+            <h2>Ristorante CoreMio<br>Küttingerstrasse 16<br>5000 Aarau<br>Aargau<br><a href="https://coremio.ch/">Website</a> </h2>
+        </div>
+        <div class="index-col-right" style="width: 80%">
+            <!--EDIT HERE-->
+            <p>Schon beim Anblick der Website coremio.ch steigt die Vorfreude auf die italienische Küche. Wir durften für das neugegründete Restaurant in Aarau einen Webauftritt umsetzen. Besonderes Augenmerk wurde auf die Präsentation des Menus gelegt. Zudem wurde eine teilautomatisierte Reservationsmöglichkeit verbaut, die das Team im Restaurantalltag entlasten wird.</p>
+        </div>
+        </div>
+    </div>
+</div>
+<br>
+<?php include 'footer.php' ?>
+</body>
+</html>
+<script type="text/javascript" src="/Website/script.js"></script>
+
