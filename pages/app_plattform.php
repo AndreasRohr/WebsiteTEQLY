195ce241bc3424f04bf457938a8466e62e2e3d09
jannik1995 <50104987+jannik1995@users.noreply.github.com>
2022-04-12 12:51:53 +0200

    App-Plattform

---
+++ pages/app_plattform.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'header_imports.php' ?>
+    <title>TEQLY Produkte & Services</title>
+    <?php include 'header.php' ?>
+</head>
+
+<body>
+<div id="main" style="margin-top: 7rem">
+    <div class="title-row">
+        <div class="title-left">
+            <div class="typewriter">
+                <h1 id="titleh1" class="indextitle">App-Plattform</h1>
+            </div>
+        </div>
+    </div>
+    <div class="index-container">
+        <div  class="title-right">
+            <h2>App-Plattform Untertitel</h2>
+        </div>
+        <!-- EASIT.rent Block -->
+        <div class="lineblack-index"></div>
+        <div class="products-col-top">
+            <h1>Support-Plattform</h1>
+        </div>
+        <div class="products-wrapper">
+            <div class="products-col-right">
+                <p>Support-Plattform Text</p>
+            </div>
+        </div>
+        <!-- Programmierung Block -->
+        <div class="lineblack-index"></div>
+        <div class="products-col-top">
+            <h1>Newsletter-Service</h1>
+        </div>
+        <div class="products-wrapper">
+            <div class="products-col-right">
+                <p>Der Versand von E-Mails ist nach wie vor ein gängiges Marketing-Instrument. Im Gegensatz zu anderen Newsletter-Angeboten bietet unsere Lösung, die auf dem MailWiz-Framework beruht, diverse Vorteile: eigene API-Schnittstelle, eigenes Branding, Newsletter Template Baukasten oder HTML-Templates, keine Kontakt- oder Mailbegrenzung, läuft auf jedem Webserver mit PHP und vieles mehr.
+                    Auch hier können Sie entscheiden, ob Sie das Mail-Tool bei uns hosten lassen wollen, oder ob wir TEQLY | News auf Ihrem eigenen Webserver installieren dürfen. So behalten Sie jederzeit die Kontrolle über Ihre Kundenkartei.</p>
+                <a href="#">Zum Newsletter-Service Protal</a>
+            </div>
+        </div>
+
+        </div>
+    </div>
+<br>
+<?php include 'footer.php' ?>
+</body>
+</html>
+<script type="text/javascript" src="/Website/script.js"></script>
