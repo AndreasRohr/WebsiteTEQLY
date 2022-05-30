04e4f21c62fe6357f778231bf66900067a9ee4ef
jannik1995 <50104987+jannik1995@users.noreply.github.com>
2022-05-30 17:08:06 +0200

    update vCard

--- vCard.php
+++ vCard.php
@@ -8,14 +8,14 @@
     <title>TEQLY Visitenkarte</title>
     <style>
         .card {
-            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
             transition: 0.3s;
             text-align: center;
             width: 80%;
         }
 
         .card:hover {
-            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
         }
 
         .containerCard {
@@ -27,52 +27,54 @@
 </head>
 <body>
 <div id="main">
-    <div style="margin-left: auto;margin-right: auto; text-align: center; font-size: 40px; font-weight: bold"><a class="logo" href="index.php" style="margin-left: 0rem">
+    <div style="margin-left: auto;margin-right: auto; text-align: center; font-size: 40px; font-weight: bold"><a
+                class="logo" href="index.php" style="margin-left: 0rem">
             <div>
                 <img src="assets/teqly_logo.png" alt="TEQLY Logo" width="50">
             </div>
 
-        </a>TEQLY</div>
+        </a>TEQLY
+    </div>
     <div class="three-col" style="margin-left: 3rem;margin-right: auto">
-    <div class="three-col" style="margin-right: auto">
-        <div class="three-col-block">
-            <div class="card">
-                <div class="containerCard">
-            <h1>Jannik Frei</h1>
-            <br>
-            <a style="color: black" href="mailto:jannik@teqly.ch">jannik@teqly.ch</a><br>
-                    <a style="color: black" href="tel:+41565111131">056 511 11 31</a><br>
-            <a style="color: black; font-weight: bolder" href="assets/vcard_jannik.vcf"
-               download="vcard_jannik.vcf">Kontaktdaten speichern</a>
-        </div>
+        <div class="three-col" style="margin-right: auto">
+            <div class="three-col-block">
+                <div class="card">
+                    <div class="containerCard">
+                        <h1>Jannik Frei</h1>
+                        <br>
+                        <a style="color: black" href="mailto:jannik@teqly.ch">jannik@teqly.ch</a><br>
+                        <a style="color: black" href="tel:+41565111131">056 511 11 31</a><br>
+                        <a style="color: black; font-weight: bolder" href="assets/vcard_jannik.vcf"
+                           download="vcard_jannik.vcf">Kontaktdaten speichern</a>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="three-col-block">
-            <div class="card">
-                <div class="containerCard">
-            <h1>Andreas Rohr</h1>
-            <br>
-            <a style="color: black" href="mailto:andreas@teqly.ch">andreas@teqly.ch</a><br>
-                    <a style="color: black" href="tel:+41565111132">056 511 11 32</a><br>
-            <a style="color: black; font-weight: bolder" href="assets/vcard_andreas.vcf"
-               download="vcard_andreas.vcf">Kontaktdaten speichern</a>
-        </div>
+            <div class="three-col-block">
+                <div class="card">
+                    <div class="containerCard">
+                        <h1>Andreas Rohr</h1>
+                        <br>
+                        <a style="color: black" href="mailto:andreas@teqly.ch">andreas@teqly.ch</a><br>
+                        <a style="color: black" href="tel:+41565111132">056 511 11 32</a><br>
+                        <a style="color: black; font-weight: bolder" href="assets/vcard_andreas.vcf"
+                           download="vcard_andreas.vcf">Kontaktdaten speichern</a>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="three-col-block" style="margin-bottom: 3rem">
-            <div class="card">
-                <div class="containerCard">
-                    <h1>Sandro Bütler</h1>
-                    <br>
-                    <a style="color: black" href="mailto:sandro@teqly.ch">sandro@teqly.ch</a><br>
-                    <a style="color: black" href="tel:+41565111130">056 511 11 30</a><br>
-                    <a style="color: black; font-weight: bolder" href="assets/vcard_sandro.vcf"
-                       download="vcard_sandro.vcf">Kontaktdaten speichern</a>
+            <div class="three-col-block" style="margin-bottom: 3rem">
+                <div class="card">
+                    <div class="containerCard">
+                        <h1>Sandro Bütler</h1>
+                        <br>
+                        <a style="color: black" href="mailto:sandro@teqly.ch">sandro@teqly.ch</a><br>
+                        <a style="color: black" href="tel:+41565111130">056 511 11 30</a><br>
+                        <a style="color: black; font-weight: bolder" href="assets/vcard_sandro.vcf"
+                           download="vcard_sandro.vcf">Kontaktdaten speichern</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-        <div class="three-col-block" >
+        <div class="three-col-block">
             <div class="card">
                 <div class="containerCard">
                     <h1>Support</h1>
@@ -96,6 +98,7 @@
                 </div>
             </div>
         </div>
+        <br>
     </div>
 </div>
 </div>
