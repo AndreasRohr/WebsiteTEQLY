f20a42a0d3435f7935cde9540b9a90a8fb528d1f
jannik1995 <50104987+jannik1995@users.noreply.github.com>
2022-05-30 16:19:37 +0200

    update value to "Vorname"

--- de/structure_footer.php
+++ de/structure_footer.php
@@ -88,7 +88,7 @@
                                     "EMAIL" id="EMAIL" />
                                 </div>
                                 <div class="form-group field-text wrap-fname">
-                                    <label for="FNAME">Vorname</label> <input class="form-control field-fname field-type-text" placeholder="Name" type="text" value="" name="FNAME" id="FNAME" />
+                                    <label for="FNAME">Vorname</label> <input class="form-control field-fname field-type-text" placeholder="Vorname" type="text" value="" name="FNAME" id="FNAME" />
                                 </div>
                                 <div class="form-group field-text wrap-lname">
                                     <label for="LNAME">Nachname</label> <input class="form-control field-lname field-type-text" placeholder="Nachname" type="text" value="" name="LNAME" id="LNAME" />
